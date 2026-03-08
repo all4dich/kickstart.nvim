747b9fc1648e4cebc40ddbfa8d98efcd08197531
Sunjoo Park <all4dich@gmail.com>
2026-03-08 10:46:04 +0900

    feat(copilot): configure custom keybindings
    - Disable default Tab mapping for better control
    - Add Ctrl+J to accept suggestions
    - Add Alt+] and Alt+[ to cycle through suggestions
    - Add Ctrl+] to dismiss suggestions
    
    Keybindings configured in init function to load before plugin
    activation.

--- lua/custom/plugins/github-copilot.lua
+++ lua/custom/plugins/github-copilot.lua
@@ -1,3 +1,14 @@
 return {
-    "github/copilot.vim",
+  "github/copilot.vim",
+  init = function()
+    vim.g.copilot_no_tab_map = true
+    vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      silent = true,
+    })
+    vim.keymap.set("i", "<M-]>", "<Plug>(copilot-next)")
+    vim.keymap.set("i", "<M-[>", "<Plug>(copilot-previous)")
+    vim.keymap.set("i", "<C-]>", "<Plug>(copilot-dismiss)")
+  end,
 }
